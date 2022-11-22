482be75850979101276eb8c8cd56dcccc33d93d1
sofi-innosonian <sofi@innosonianmakers.com>
2022-11-22 16:05:19 +0900

    Vacation model에 created_at, deleted_at, message field 추가

--- vacations/models.py
+++ vacations/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class Vacation(models.Model):
     user_name = models.CharField(max_length=100)
     date = models.DateTimeField()
-
+    message = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_created=True)
+    deleted_at = models.DateTimeField(null=True)
 
