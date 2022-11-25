c3ccb2cc1ab90bc456c40d7a4f3d80ef6c2a8132
ria-innosonian <skysky5021@naver.com>
2022-11-25 19:34:11 +0900

    Feat: Date formatter 분리(date, 요일) & slack response form 수정

--- vacations/views.py
+++ vacations/views.py
@@ -5,16 +5,18 @@ from vacations.serializers import VacationSerializer
 from .models import Vacation, User
 from datetime import datetime, date
 import dateutil.parser
+import json
 
-def timezone_parser(timezone_data):
-    days = ['월(Mon)','화(Tue)','수(Wed)','목(Thu)','금(Fri)','토(Sat)','일(Sun)']
+def date_formatter(timezone_data):
     dt_parse = dateutil.parser.parse(timezone_data)
-    dt_form = dt_parse.year, dt_parse.month, dt_parse.day
-    day = date(dt_parse.year, dt_parse.month, dt_parse.day).weekday()
-    final_format = f"{dt_form[0]}.{dt_form[1]}.{dt_form[2]} {days[day]}"
-    print("final_format::::", final_format )
+    date_format = date(dt_parse.year, dt_parse.month, dt_parse.day)
+    return date_format
 
-    return final_format
+def day_of_date(date_format):
+    days = ['(월)','(화)','(수)','(목)','(금)','(토)','(일)']
+    day_num = date_format.weekday()
+    day= days[day_num]
+    return day
 
 class VacationAPI(APIView):
     def post(self, request):
@@ -22,65 +24,67 @@ class VacationAPI(APIView):
         vacation = Vacation.objects.filter(user=user)
         serializer = VacationSerializer(vacation, many=True)
         result = list()
+        user_data_form = {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"*{user.name}* 님의 휴가 사용 내역: 총 *{len(vacation)}* 건"
+            }
+        }
+        devider = {
+            "type": "divider"
+        }
+        result.append(user_data_form)
+        result.append(devider)
         for data in serializer.data:
-            vacation_format = timezone_parser(data.get('start_date'))
-            print("vacation_format::::", vacation_format)
-            result.append(vacation_format)
+            start_date = date_formatter(data.get('start_date'))
+            start_day = day_of_date(start_date)
+            end_date = date_formatter(data.get('end_date'))
+            end_day = day_of_date(end_date)
 
-        vacations = '\n'.join(result)
+            if start_date == end_date:
+                vacation_form = {
+                        "type": "section",
+                        "accessory": {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "사용 완료",
+                            },
+                            "style": "primary"
+                        },
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f"{start_date} {start_day} - 1일"
+                        }
+                    }
+                result.append(vacation_form)
+            else:
+                # 일자 계산은 아직 더 다듬어야 함
+                # cal_vacations = end_date - start_date  # 반환형식: 1 day, 0:00:00
+                vacation_form = {
+                        "type": "section",
+                        "accessory": {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "사용 완료",
+                            },
+                            "style": "primary"
+                        },
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f"{start_date} {start_day} ~ {end_date} {end_day} - 2일간 "
+                        }
+                    }
+                print("vacation_form type:::::", type(vacation_form))
+                result.append(vacation_form)
+        # list로 반환되는 것이 문제였는데, 반대로 이용하기로 함( "blocks" 키 값의 value를 리스트[]로 싸는 형식이라 거기서 result를 불러준다.
         form = {
             "response_type": "in_channel",
             "attachments": [{
                 "color": "#2eb886",
-                "blocks": [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": f"*{user.name}* 님의 휴가 사용 내역: *{len(vacation)}*"
-                        }
-                    },
-                    {
-                        "type": "divider"
-                    },
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": f"{vacations}"
-                        }
-                    },
-                    {
-                        "type": "section",
-                        "accessory": {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "사용 완료",
-                            },
-                            "style": "primary"
-                        },
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "2022.11.21 (금) - 1일"
-                        }
-                    },
-                    {
-                        "type": "section",
-                        "accessory": {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "사용 완료",
-                            },
-                            "style": "primary"
-                        },
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "2022.11.21 (금) ~ 11.22 (토) - 2일"
-                        }
-                    }
-                ]
+                "blocks": result
             }]
         }
         return Response(data=form, status=status.HTTP_200_OK)
