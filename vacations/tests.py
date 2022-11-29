b5e2c4c169400f5c6389b6b3153df454f59ca743
ria-innosonian <skysky5021@naver.com>
2022-11-29 19:23:04 +0900

    test 작성 및 동일 데이터 append 현상 fix (문제 원인 파악)

--- vacations/tests.py
+++ vacations/tests.py
@@ -1,40 +1,105 @@
+from collections import OrderedDict
+
 from django.test import TestCase
+from . import views
 
-# Create your tests here.
+
+class User():
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __str__(self):
+        return self.name
 
 
-big_result1 = list()
-big_result2 = list()
+class Vacation:
+    def __init__(self, user=None, start_date=None, end_date=None, message=None, created_at=None, deleted_at=None):
+        self.user = user
+        self.start_date = start_date
+        self.end_date = end_date
+        self.message = message
+        self.created_at = created_at
+        self.deleted_at = deleted_at
 
-data = 0
-test_result = {"2" : data}
 
-#
-# print("big_result:::", big_result)  # {"2" : 0}
-#
-#
-# test_result["2"] = 1
-# big_result.append(test_result)
-#
-# print("big_result1:::", big_result) # {"2" : 1}
-#
-#
-# test_result["2"] = 2
-# big_result.append(test_result)
-#
-# print("big_result2:::", big_result) # {"2" : 2}, {"2" : 2}
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        self.maxDiff = None
 
-# 변수를 다르게 설정해서 얻어낸 결과. Q. 그렇다면 동일한 변수를 가지고 동일한 결과를 찾는 방법을 연구해볼 것
-for i in range(3):
-    globals()["key{}".format(i)] = {"2" : i }
-    big_result1.append(globals()["key{}".format(i)])
+    def test_hello(self):
+        data = [OrderedDict([('id', 3), ('created_at', '2022-11-25T07:02:40Z'), ('start_date', '2022-11-25T07:02:43Z'),
+                             ('end_date', '2022-11-25T07:02:45Z'), ('message', 'dd'),
+                             ('deleted_at', '2022-11-25T07:02:50Z'),
+                             ('user', 'U04ARD6H3PU')]),
+                OrderedDict(
+                    [('id', 4), ('created_at', '2022-11-25T07:02:58Z'), ('start_date', '2022-11-25T07:03:02Z'),
+                     ('end_date', '2022-11-26T07:03:05Z'), ('message', 'ddd'), ('deleted_at', '2022-11-25T07:03:10Z'),
+                     ('user', 'U04ARD6H3PU')]),
+                OrderedDict(
+                    [('id', 5), ('created_at', '2022-11-28T01:58:44Z'), ('start_date', '2022-11-28T01:58:51Z'),
+                     ('end_date', '2023-01-01T01:59:04Z'), ('message', 'ㅇㅇㅇ'), ('deleted_at', '2022-11-28T01:59:10Z'),
+                     ('user', 'U04ARD6H3PU')]),
+                OrderedDict(
+                    [('id', 6), ('created_at', '2022-11-28T03:07:20Z'), ('start_date', '2022-12-01T03:07:30Z'),
+                     ('end_date', '2022-12-01T03:07:39Z'), ('message', 'dd'), ('deleted_at', '2022-11-28T03:07:42Z'),
+                     ('user', 'U04ARD6H3PU')])]
 
-print("big_result1_for::::", big_result1)
+        vacation_api = views.VacationAPI()
+        user = User(1, "Ria")
+        result = vacation_api.generate_from_data(data, user)
+        expected = {"response_type": "in_channel",
+                    'attachments':
+                        [{'color': '#2eb886',
+                          'blocks':
+                                [{'type': 'section',
+                                  'text': {'text': '*Ria* 님의 휴가 사용 내역: 총 *4* 건, 사용 일수: 총 *39* 일',
+                                           'type': 'mrkdwn'},
+                                  },
+                                 {'type': 'divider'},
+                                 {'type': 'section',
+                                  'accessory': {'type': 'button',
+                                                'text': {'type': 'plain_text',
+                                                         'text': '사용완료'}
+                                                },
+                                  'text': {'type': 'mrkdwn',
+                                           'text': '2022-11-25(금) - 1day'}
+                                  },
+                                  {'type': 'section',
+                                   'accessory': {'type': 'button',
+                                                 'text': {'type': 'plain_text',
+                                                          'text': '사용완료'}
+                                                 },
+                                   'text': {'type': 'mrkdwn',
+                                            'text': '2022-11-25(금) ~ 2022-11-26(토) - 2days'}
+                                   },
+                                   {'type': 'section',
+                                    'accessory': {'type': 'button',
+                                                  'text': {'type': 'plain_text',
+                                                           'text': '사용완료'}
+                                                  },
+                                    'text': {'type': 'mrkdwn',
+                                             'text': '2022-11-28(월) ~ 2023-01-01(일) - 35days'}
+                                    },
+                                    {'type': 'section',
+                                     'accessory': {'type': 'button',
+                                                   'text': {'type': 'plain_text',
+                                                            'text': '사용대기'},
+                                                   'style': 'primary'
+                                                   },
+                                     'text': {'type': 'mrkdwn',
+                                              'text': '2022-12-01(목) - 1day'}
+                                     }
+                                    ],
+                                }
+                         ],
+                    }
 
-# Q. 그렇다면 동일한 변수를 가지고 동일한 결과를 찾는 방법을 연구해볼 것 // 그치만 불가능. 왜냐면 slack에서 지정한 key값을 넘겨줘야 하기 때문에.
-for i in range(3):
-    test_result = {f"{i}" : i}
-    big_result2.append(test_result)
+        self.assertEquals(expected, result)
+        print('hello???')
 
-print("big_result2_for::::", big_result2)
+
+
+
+
 
--- vacations/views.py
+++ vacations/views.py
@@ -7,102 +7,75 @@ from datetime import datetime, date
 import dateutil.parser
 import json
 
+
 def date_formatter(timezone_data):
     dt_parse = dateutil.parser.parse(timezone_data)
     date_format = date(dt_parse.year, dt_parse.month, dt_parse.day)
     return date_format
 
+
 def day_of_date(date_format):
-    days = ['(월)','(화)','(수)','(목)','(금)','(토)','(일)']
+    days = ['(월)', '(화)', '(수)', '(목)', '(금)', '(토)', '(일)']
     day_num = date_format.weekday()
     day= days[day_num]
     return day
+
 
 class VacationAPI(APIView):
     def post(self, request):
         user = User.objects.get(id=request.data.get('user_id'))
         vacation = Vacation.objects.filter(user=user)
         serializer = VacationSerializer(vacation, many=True)
+        form = self.generate_from_data(serializer.data, user)
+        print("form:::", form)
+        return Response(data=form, status=status.HTTP_200_OK)
+
+    def generate_from_data(self, serialized_vacation, user):
         result = list()
         total_vacations = list()
 
-        # 각 휴가의 사용 일자 총합을 구하기 번거로우니 '사용일수' 컬럼을 하나 파면 어떨까? 휴가 신청 시에 연산해서 테이블에 담고 -> 모델에서 가져와서 sum만 하면 되도록
-        # 현재는 list 새로 파고 for문을 따로 돌려서 sum하는 형식(user_data_form, divider가 하위 for문 안에서 같이 돌면 안되고, user_data_form에 sum값을 주어야 해서)
-        # for difference in serializer.data:
-        #     cal_vacations = (date_formatter(difference.get('end_date')) - date_formatter(difference.get('start_date'))).days + 1
-        #     total_vacations.append(cal_vacations)
-
-        section_text = { "type" : "mrkdwn", "text": ""}
-        user_data_form = { "type" : "section", "text" : section_text }
-        # user_data_form = {
-        #     "type": "section",
-        #     "text": {
-        #         "type": "mrkdwn",
-        #         "text": f"*{user.name}* 님의 휴가 사용 내역: 총 *{len(vacation)}* 건, 사용 일수: 총 *{sum(total_vacations)}* 일"
-        #     }
-        # }
-        divider = {
-            "type": "divider"
-        }
+        section_text = {"type": "mrkdwn", "text": ""}
+        user_data_form = {"type": "section", "text": section_text}
+        divider = {"type": "divider"}
         result.append(user_data_form)
         result.append(divider)
 
-        button_text = {"type": "plain_text", "text": ""}
-        button = {"type": "button", "text": button_text}
-        vacation_form = {"type": "mrkdwn", "text": ""}
-        vacation_list_form = {"type": "section", "accessory": button, "text": vacation_form}
+        for data in serialized_vacation:
+            button_text = {"type": "plain_text", "text": ""}
+            button = {"type": "button", "text": button_text}
+            vacation_form = {"type": "mrkdwn", "text": ""}
+            vacation_list_form = {"type": "section", "accessory": button, "text": vacation_form}
 
-        for data in serializer.data:
             start_date = date_formatter(data.get('start_date'))
             start_day = day_of_date(start_date)
             end_date = date_formatter(data.get('end_date'))
             end_day = day_of_date(end_date)
 
+            # 각 휴가의 사용 일자 연산이 조회에서만 필요한가? vacation table에 '사용일수' 컬럼을 하나 파면 어떨까?
+            # 휴가 신청 시에 연산해서 테이블에 담고 -> 모델에서 가져와서 sum만 하면 되도록
             cal_vacations = (end_date - start_date).days + 1  # 반환형식: 숫자
             total_vacations.append(cal_vacations)
-            section_text['text'] = f"*{user.name}* 님의 휴가 사용 내역: 총 *{len(vacation)}* 건, 사용 일수: 총 *{sum(total_vacations)}* 일"
 
-            try:
-                if datetime.today() >= datetime(start_date.year, start_date.month, start_date.day, hour=11):
-                    button_text["text"] = "사용완료"
-                else:
-                    button_text["text"] = "사용대기"
-                    button_style = {"style" : "primary"}
-                    button.update(button_style)
-            except:
-                print("error, 날짜 데이터 연산에 실패했습니다. 데이터를 확인해 주세요.")
+            if datetime.today() >= datetime(start_date.year, start_date.month, start_date.day, hour=11):
+                button_text["text"] = "사용완료"
 
-            finally:
-                if start_date == end_date:
-                    print("start_date == end_date 몇번 타나")
-                    print("data.get('id'):::::", data.get('id'))
-                    globals()['vacation_list_form{}'.format(data.get('id'))] = {"type": "section", "accessory": button,
-                                                                                "text": vacation_form}
-                    globals()['vacation_list_form{}'.format(data.get('id'))]["text"]["text"] = f"{start_date}{start_day} - {cal_vacations}day"
-                    print(f"vacation_list_form_{data.get('id')}:::::", globals()['vacation_list_form{}'.format(data.get('id'))])
-                    result.append(globals()['vacation_list_form{}'.format(data.get('id'))])
+            else:
+                button_text["text"] = "사용대기"
+                button_style = {"style": "primary"}
+                button.update(button_style)
 
-                    # 기존 코드
-                    # vacation_form["text"] = f"{start_date}{start_day} - {cal_vacations}day"
-                    # print("== vacation_list_form :::/n", vacation_list_form)
-                    # result.append(vacation_list_form)
+            if start_date == end_date:
+                vacation_form["text"] = f"{start_date}{start_day} - {cal_vacations}day"
+                result.append(vacation_list_form)
 
-                else:
-                    print("start_date <> end_date 몇번 타나")
-                    print("data.get('id'):::::", data.get('id'))
-                    globals()['vacation_list_form{}'.format(data.get('id'))] = {"type": "section", "accessory": button,
-                                                                                "text": vacation_form}
-                    globals()['vacation_list_form{}'.format(data.get('id'))]["text"]["text"] = f"{start_date}{start_day} ~ {end_date}{end_day} - {cal_vacations}days "
-                    print(f"vacation_list_form_{data.get('id')}:::::", globals()['vacation_list_form{}'.format(data.get('id'))])
-                    result.append(globals()['vacation_list_form{}'.format(data.get('id'))])
+            else:
+                vacation_form["text"] = f"{start_date}{start_day} ~ {end_date}{end_day} - {cal_vacations}days"
+                result.append(vacation_list_form)
 
-                    # 기존 코드
-                    # vacation_form["text"] = f"{start_date}{start_day} ~ {end_date}{end_day} - {cal_vacations}days "
-                    # print("<> vacation_list_form :::/n", vacation_list_form)
-                    # result.append(vacation_list_form)
+        section_text['text'] = f"*{user.name}* 님의 휴가 사용 내역: 총 *{len(serialized_vacation)}* 건, 사용 일수: 총 *{sum(total_vacations)}* 일"
 
-
-        # result가 list여서 꺼내는게 문제였는데, 반대로 리스트인걸 이용하기로 함("blocks" 키 값의 value를 리스트[]로 싸는 형식이라 거기서 result를 불러준다.
+        # result가 list여서 꺼내는게 문제였는데, 반대로 리스트인걸 이용하기로 함
+        # "blocks" 키 값의 value를 리스트[]로 싸는 형식이라 거기서 result를 불러준다.
         form = {
             "response_type": "in_channel",
             "attachments": [{
@@ -110,7 +83,9 @@ class VacationAPI(APIView):
                 "blocks": result
             }]
         }
-        print("form:::", form)
-        return Response(data=form, status=status.HTTP_200_OK)
+        return form
+
+    def generate_payload(self, data):
+        return data
 
 
