385861f1aa68c553d53f0ab652194ebb879cbffe
sofi-innosonian <sofi@innosonianmakers.com>
2022-12-01 11:39:03 +0900

    Remove slack.py file

--- vacations/slack.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def slack_notify(token, channel, text):
-    response = requests.post("https://slack.com/api/chat.postMessage",
-                             headers={"Authorization": "Bearer " + token},
-                             data={"channel": channel, "text": text}
-                             )
-    print(response)
